b91fee92991af85eba6303d2490e4566e92c9250
Marwan Rabbâa <waghanza@gmail.com>
2016-03-08 22:06:52 +0100

    list all records in plain text

--- src/forest.cr
+++ src/forest.cr
@@ -7,6 +7,17 @@ module Forest
   dsn = ARGV[0]
   db = PG.connect(dsn)
 
+  get "/:table" do |env|
+    table = env.params.url["table"]
+    begin
+      result = db.exec("select * from #{table}")
+    rescue e
+      env.response.status_code = 404
+    else
+      results.to_hash
+    end
+  end
+
   # Close connection at exit
   at_exit do
     Signal::INT.trap {
