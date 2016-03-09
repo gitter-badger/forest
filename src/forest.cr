92e02a61eca788588192cfb6a842cdd02cb5771d
Marwan Rabbâa <waghanza@gmail.com>
2016-03-09 12:59:02 +0100

    deal with unreachable connection

--- src/forest.cr
+++ src/forest.cr
@@ -3,25 +3,28 @@ require "pg"
 require "./forest/*"
 
 module Forest
-  # Init connection
   dsn = ARGV[0]
-  db = PG.connect(dsn)
+  begin
+    database = PG.connect(dsn)
+  rescue e : PG::ConnectionError
+    exit 1
+  end
 
   get "/:table" do |env|
     table = env.params.url["table"]
     begin
-      result = db.exec("select * from #{table}")
-    rescue e
+      result = database.exec("select * from #{table}")
+    rescue 
       env.response.status_code = 404
     else
-      results.to_hash
+      result.to_hash
     end
   end
 
   # Close connection at exit
   at_exit do
     Signal::INT.trap {
-      db.finish
+      database.finish if database
     }
   end
 end
